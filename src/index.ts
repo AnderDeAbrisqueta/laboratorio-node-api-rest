2e3b8c614f77fcf0bbbf9d9a633716601fc42d6c
Ander De Abrisqueta Rangel <ander.deabrisqueta@gmail.com>
2023-09-02 16:00:34 +0200

    insert-update implementation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import express, { query } from "express";
-import { getHouse, getHouseList } from "./mock-db";
+import express from "express";
+import { getHouse, getHouseList, insertHouse, updateHouse } from "./mock-db";
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Test");
@@ -17,6 +18,20 @@ app.get("/api/houses/:id", async (req, res) => {
   const houseId = Number(id);
   const house = await getHouse(houseId);
   res.send(house);
+});
+
+app.post("/api/houses", async (req, res) => {
+  const house = req.body;
+  const newhouse = await insertHouse(house);
+  res.status(201).send(newhouse);
+});
+
+app.put("/api/houses/:id", async (req, res) => {
+  const { id } = req.params;
+  const houseId = Number(id);
+  const house = req.body;
+  await updateHouse(houseId, house);
+  res.sendStatus(204);
 });
 
 app.listen(3000, () => {
