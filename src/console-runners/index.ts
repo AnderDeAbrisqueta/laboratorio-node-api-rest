09c8f62b2cd6c39cdafb0cad8c2b44be2f908b03
Ander De Abrisqueta Rangel <ander.deabrisqueta@gmail.com>
2023-09-03 22:21:43 +0200

    Console-runners

--- src/console-runners/index.ts
+++ src/console-runners/index.ts
@@ -1,1 +1,19 @@
+const inquirer = require("inquirer");
 
+(async () => {
+  let exit = false;
+  while (!exit) {
+    const answer = await inquirer.prompt({
+      name: "consoleRunner",
+      type: "list",
+      message: "Which console-runner do you want to run?",
+      choices: ["create-admin", "exit"],
+    });
+
+    if (answer.consoleRunner !== "exit") {
+      console.log("Create admin runner");
+    } else {
+      exit = true;
+    }
+  }
+})();
