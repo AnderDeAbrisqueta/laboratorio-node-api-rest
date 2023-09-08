bfc8de2c584d402fe09b80b70e951935578f1ec3
Ander De Abrisqueta Rangel <ander.deabrisqueta@gmail.com>
2023-09-08 21:18:09 +0200

    mongoose implementation

--- src/dals/house/house.context.ts
+++ src/dals/house/house.context.ts
@@ -1,4 +1,13 @@
-import { db } from "core/servers";
+import { model, Schema } from "mongoose";
 import { House } from "./house.model";
 
-export const getHouseContext = () => db?.collection<House>("houses");
+const houseSchema = new Schema<House>({
+  title: { type: Schema.Types.String, required: true },
+  image: { type: Schema.Types.String, required: true },
+  description: { type: Schema.Types.String, required: true },
+  room_number: { type: Schema.Types.Number, required: true },
+  bed_number: { type: Schema.Types.Number, required: true },
+  bath_number: { type: Schema.Types.Number, required: true },
+});
+
+export const houseContext = model<House>("houses", houseSchema);
--- src/dals/house/repositories/house.db-repository.ts
+++ src/dals/house/repositories/house.db-repository.ts
@@ -1,32 +1,38 @@
 import { ObjectId } from "mongodb";
 import { HouseRepository } from "./house.repository";
 import { House } from "../house.model";
-import { getHouseContext } from "../house.context";
+import { houseContext } from "../house.context";
 
 export const dbRepository: HouseRepository = {
   getHouseList: async (page?: number, pageSize?: number) => {
     const skip = Boolean(page) ? (page - 1) * pageSize : 0;
     const limit = Boolean(pageSize) ? pageSize : 0;
-    return await getHouseContext().find().skip(skip).limit(limit).toArray();
+    return await houseContext.find().skip(skip).limit(limit).lean();
   },
   getHouse: async (id: string) => {
-    return await getHouseContext().findOne({
-      _id: new ObjectId(id),
-    });
+    return await houseContext
+      .findOne({
+        _id: new ObjectId(id),
+      })
+      .lean();
   },
   saveHouse: async (house: House) => {
-    return await getHouseContext().findOneAndUpdate(
-      {
-        _id: house._id,
-      },
-      { $set: house },
-      { upsert: true, returnDocument: "after" }
-    );
+    return await houseContext
+      .findOneAndUpdate(
+        {
+          _id: house._id,
+        },
+        { $set: house },
+        { upsert: true, returnDocument: "after" }
+      )
+      .lean();
   },
   deleteHouse: async (id: string) => {
-    const { deletedCount } = await getHouseContext().deleteOne({
-      _id: new ObjectId(id),
-    });
+    const { deletedCount } = await houseContext
+      .deleteOne({
+        _id: new ObjectId(id),
+      })
+      .lean();
     return deletedCount === 1;
   },
 };
