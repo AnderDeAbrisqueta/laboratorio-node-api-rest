5ef2c15e318903cf9c7dd606e8657bb1f6ea4f3f
DOM2\ADR <ander.de@mayoral.com>
2024-03-22 12:03:19 +0100

    Created Unit test, and config file

---
+++ src/pods/house/house.mappers.spec.ts
@@ -0,0 +1,20 @@
+import {
+    mapHouseFromModelToApi,
+    mapHouseListFromModelToApi,
+    mapHouseFromApiToModel,
+    mapReviewFromModelToApi,
+    mapReviewsFromModelToApi,
+    mapReviewFromApiToModel,
+  } from './house.mappers';
+  
+  describe('house mappers spec', () => {
+    it("should pass spect", () => {
+      //Arrange
+
+      //Act
+
+      //Assert
+      expect(true).toBeTruthy();
+    });
+  });
+  
